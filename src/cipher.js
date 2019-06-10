78e8218b7e38e35117b03bf3184e0e1905d851ef
GalioV <galilea.valle@gmail.com>
2019-06-09 22:39:07 -0500

    cifrado hecho

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,12 @@
 window.cipher = {
  encode: function(string, offset) {
-      let x = cifradito.value;
+      let x = letritas.value;
       var result = "";
           for (let i = 0; i < x.length; i++) {
               let c = x[i].charCodeAt(0);
 
                   if(c >= 97 && c <= 122){
-                    let form = (c - 97 + parseInt(offset.value)) % 26 + 97;
+                    let form = (c - 97 + parseInt(desplazamiento.value)) % 26 + 97;
                     let letritas = String.fromCharCode(form);
                     result += letritas;
                   }}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const showCifrado = document.getElementById("boton2");
+const showCifrado = document.getElementById("boton2");//tomar el id del botón
 const showDecifrado = document.getElementById("boton3");
 
-//addEventListeners
+//addEventListener para escribir un objeto
 showCifrado.addEventListener("click", function() {console.log(window.cipher.encode())});
-showDecifrado.addEventListener("click", function() {console.log(window.cipher.decode())});
 
 
 function cifradito(){
-  document.getElementById("letritas").innerHTML= window.cipher.encode(string, offset);
+  document.getElementById("letrotas").innerHTML= window.cipher.encode();
+  //va a escribir en el campo letritas un Objeto
+
 }
+
 function decifradito(){
   document.getElementById("letritas").innerHTML= "texto_decifradito";
 }
